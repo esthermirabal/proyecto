fad7c06cc895c209cb391ea20ed6063dbcd35363
Esther Mirabal <esther.mirabal.1402@gmail.com>
2023-05-18 20:37:19 -0300

    insertamos nuevos cambios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './componente/navbar/navbar.component';
+import { DashboardComponent } from './componente/dashboard/dashboard.component';
+import { FooterComponent } from './componente/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    DashboardComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
